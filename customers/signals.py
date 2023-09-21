64bab9272fca8c483f0e266aceab1f2954f02fff
Gafur Sharipov <63968483+Feyalon@users.noreply.github.com>
2023-09-21 16:16:26 +0500

    Update signals.py

--- customers/signals.py
+++ customers/signals.py
@@ -5,7 +5,7 @@ from django.core.mail import send_mail
 from robots.models import Robot
 
 @receiver(post_save, sender=Robot)
-def notify_robot_availability(sender, instance, **kwargs):
+def notify_robot_availability(sender, instance, **kwargs): # сразу тут пишу что я не SMTP не настраивал.
     if instance.avaible:
         subject = "Ваш робот доступен в наличии"
         message = f"Добрый день!\n\nНедавно вы интересовались нашим роботом модели {instance.model}, версии {instance.version}.\nЭтот робот теперь в наличии. Если вам подходит этот вариант - пожалуйста, свяжитесь с нами."
